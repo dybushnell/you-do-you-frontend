c3f70be000413d3c4d506e928d51d296454d9f4e
Dave Bushnell <dybushnell@gmail.com>
2019-09-19 10:08:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import ProfileContainer from "./containers/ProfileContainer";
+import MatchContainer from "./containers/MatchContainer";
+import MessagesContainer from "./containers/MessagesContainer";
+import WelcomeContainer from "./containers/WelcomeContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Button from "react-rainbow-components/components/Button";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <div className="welcome">
+                <div>
+                  <WelcomeContainer />
+                </div>
+              </div>
+            );
+          }}
+        />{" "}
+        {/* close Welcome route */}
+        <Route
+          exact
+          path="/matches"
+          render={() => {
+            return (
+              <div className="matches">
+                <div>
+                  <MatchContainer />
+                </div>
+              </div>
+            );
+          }}
+        />{" "}
+        {/* close Matches route */}
+        <Route
+          exact
+          path="/messages"
+          render={() => {
+            return (
+              <div className="messages">
+                <div>
+                  <MessagesContainer />
+                </div>
+              </div>
+            );
+          }}
+        />{" "}
+        {/* close Messages route */}
+        <Route
+          exact
+          path="/profile"
+          render={() => {
+            return (
+              <div className="profile">
+                <div>
+                  <ProfileContainer />
+                </div>
+              </div>
+            );
+          }}
+        />{" "}
+        {/* close Profile route */}
+      </Router>
+    );
+  }
 }
+
+/*     <Button
+        label="Hello!"
+        variant="brand"
+        onClick={() => alert("Hello World!")}
+        /> */
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
 
 export default App;
---
+++ src/containers/WelcomeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WelcomeContainer() {
+  return (
+    <div>
+      <h1>You Do You</h1>
+      <div className="welcome-container">Login Signup</div>
+    </div>
+  );
+}
+
+export default WelcomeContainer;
